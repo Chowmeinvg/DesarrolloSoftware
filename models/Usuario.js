d228e772d374acf0b92f26f67b79af550d00a6fc
Jimmy <guillermo.villa@uabc.edu.mx>
2025-05-06 14:20:40 -0700

    Reestructura de carpetas para organizar cada microservicio

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,13 +1,28 @@
 const mongoose = require('mongoose');
 
-const usuarioSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  correo: { type: String, required: true, unique: true },
-  rol: { type: String, enum: ['estudiante', 'creador', 'administrador'], default: 'estudiante' },
-  idioma: { type: String, enum: ['es', 'en'], default: 'es' },
-  creadoEn: { type: Date, default: Date.now }
-});
+const userSchema = new mongoose.Schema({
+    nombre: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    correo: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    rol: {
+        type: String,
+        enum: ['estudiante', 'creador', 'administrador'],
+        default: 'estudiante',
+    },
+    idioma: {
+        type: String,
+        enum: ['es', 'en'],
+        default: 'es',
+    },
+    }, { timestamps: true });
 
-const Usuario = mongoose.model('Usuario', usuarioSchema);
-
-module.exports = Usuario;
+module.exports = mongoose.model('Usuario', userSchema);
